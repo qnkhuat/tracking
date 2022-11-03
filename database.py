5d8b409323e541a211ae891b3b8aea2e181361d2
Ngoc Khuat <qn.khuat@gmail.com>
2022-11-03 22:35:12 +0700

    I think I figured out the migrations

--- database.py
+++ database.py
@@ -1,2 +1,28 @@
+from contextlib import contextmanager
 import sqlite3
-conn = sqlite3.connect("test.db")
+
+DB = sqlite3.connect("test.db")
+
+def add_data(conn, name, value, timestamp):
+  sql = """INSERT INTO data
+  (name, value, timestamp)
+  VALUES
+  (?, ?, ?)
+  """
+  params = [name, value, timestamp]
+  return conn.execute(sql, params)
+
+@contextmanager
+def connection():
+  """
+  Connection context manager that conviently rollback if bad things happen, otherwise commits the result.
+
+  with connection() as conn:
+    conn.execute("insert into data values ('name')")
+  """
+  try:
+    yield DB
+  except Exception:
+    DB.rollback()
+  else:
+    DB.commit()
--- migrations.py
+++ migrations.py
@@ -1,18 +1,34 @@
-from database import conn
+from database import connection, DB
 
-CREATE_MIGRATION_TABLE_IF_NEEDED = """
-CREATE TABLE IF NOT EXISTS migration (
-  name text NOT NULL,
-  created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP);
-"""
+def create_migration_table_if_needed():
+  sql = """
+  CREATE TABLE IF NOT EXISTS migration (
+    name text NOT NULL,
+    created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP);
+  """
 
-conn.execute(CREATE_MIGRATION_TABLE_IF_NEEDED)
+  return DB.execute(sql)
 
+def save_migration_sql(name):
+  return ["INSERT INTO migration values (?);", [name]]
 
+def run_migrations(migrations):
+  create_migration_table_if_needed()
 
+  for migration in migrations:
+    migration_name = migration['name']
+    is_migration_exists = DB.execute("SELECT count(*) FROM migration WHERE name = ?;",
+                                       [migration_name]).fetchone()[0]
+    if is_migration_exists == 0:
+      print(f"Running migration: {migration_name}")
+      with connection() as conn:
+        if conn.execute(migration["sql"]).fetchone() is None:
+          conn.execute("INSERT INTO migration (name) values (?);",
+                              [migration_name]).fetchall()
+    else:
+      print(f"Skip migration {migration_name}")
 
-
-
+## ------------------------------------- Migrations definition ------------------------------------- ##
 
 create_data_table = {
     "name": "create_data_table",
@@ -22,24 +38,11 @@ create_data_table = {
     value REAL,
     timestamp datetime NOT NULL,
     created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP)
-    """
-    }
+    """}
 
-
-
-
-
-
-## ------------------------------------- Execute migrations ------------------------------------- ##
 migrations = [
     create_data_table,
     ]
 
-
-for migration in migrations:
-  is_migration_exists = conn.execute("select count(*) from migration where name = ?", [migration["name"]]).fetchone()[0]
-  if is_migration_exists == 0:
-    continue
-  else:
-    conn.execute(migration["sql"])
-
+if __name__ == "__main__":
+  run_migrations(migrations)
