f8c78e914822a645f963c775b5286a0ff30e4192
Ngoc Khuat <qn.khuat@gmail.com>
2022-11-04 18:55:42 +0700

    add the schedule job

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+import time
+import schedule
+
 from tracking import database as db
 from migrations import run_migrations
 
+from tracking.tasks.daily_sjc_gold import daily_sjc_gold
+
+def schedule_tasks():
+  print("Scheduling tasks")
+  schedule.every().day.at("00:00").do(daily_sjc_gold)
+
 def main ():
   run_migrations()
+  schedule_tasks()
 
+  while True:
+    schedule.run_pending()
+    time.sleep(1)
 
 if __name__ == "__main__":
   main()
--- tracking/tasks/daily_sjc_gold.py
+++ tracking/tasks/daily_sjc_gold.py
@@ -11,7 +11,7 @@ from tracking.tasks.base import runner
 from tracking import database as db
 
 @runner("daily_sjc_gold")
-def run(**kwargs):
+def daily_sjc_gold(**kwargs):
   url = "https://sjc.com.vn/giavang/"
 
   chrome_options = Options()
@@ -19,17 +19,25 @@ def run(**kwargs):
   driver = webdriver.Chrome(options=chrome_options)
   driver.get(url)
   try:
-    element = WebDriverWait(driver, 10).until(
+    buy_price_el = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div[2]/div[2]/table/tbody/tr[2]/td[2]/span")))
+    sell_price_el = driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div[2]/table/tbody/tr[2]/td[3]/span")
   except TimeoutException:
     raise Exception("FAILED TO FIND ELEMENT")
 
   with db.connection() as conn:
-    value = int(element.text.replace(",", ""))
-    data ={"name": "sjc_gold_buy",
-           "value": value,
-           "type": "gold",
-           "settings": json.dumps({"currency": "VND"}),
-           "timestamp": datetime.now()}
+    buy_price = int(buy_price_el.text.replace(",", ""))
+    data = {"name": "sjc_gold_buy",
+            "value": buy_price,
+            "type": "gold",
+            "settings": json.dumps({"currency": "VND", "unit": "1luong"}),
+            "timestamp": datetime.now()}
     conn.execute(*db.insert_sql("data", data))
-run()
+
+    sell_price = int(sell_price_el.text.replace(",", ""))
+    data = {"name": "sjc_gold_sell",
+            "value": sell_price,
+            "type": "gold",
+            "settings": json.dumps({"currency": "VND"}),
+            "timestamp": datetime.now()}
+    conn.execute(*db.insert_sql("data", data))
